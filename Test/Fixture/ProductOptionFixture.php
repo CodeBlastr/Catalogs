bf87f2532aa88e72613ef476a04a5309f9ddddba
Richard Kersey <github@razorit.com>
2013-04-27 14:54:29 -0700

    Added Product Option Fixture

---
+++ Test/Fixture/ProductOptionFixture.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * ProductOptionFixture
+ *
+ */
+class ProductOptionFixture extends CakeTestFixture {
+	
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('config' => 'Products.ProductOption');
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array();
+}
